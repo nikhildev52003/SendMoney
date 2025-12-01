c714905d885418117a94e664ed34adacd1979745
nikhildev52003 <nikhil.dev52003@gmail.com>
2025-12-01 13:54:24 +0530

    Fix port

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,21 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { router } from "./router/user.js";
 import { routerr } from "./router/account.js";
+
+dotenv.config();
+
 const PORT = process.env.PORT || 1000;
+
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", process.env.CLIENT_URL],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
 };
-dotenv.config();
 app.use(cors(corsOptions));
 app.use(express.json());
+
+
 db(process.env.MONGO_URL);
 
 app.use("/api/v1/user", router);
